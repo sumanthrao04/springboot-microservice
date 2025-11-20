629c0137c462b506e246c454ae0195518de707af
sumanthrao04 <sumanthrao75@gmail.com>
2025-11-20 18:13:13 +0530

    config server with refresh scope

---
+++ config-server/config-server/src/test/java/com/sumath/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumath.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
